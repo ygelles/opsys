563b2a81d93104c6292da105687ec92edbdcb9bb
eliawei <39025262+eliawei@users.noreply.github.com>
2020-01-08 13:37:34 +0200

    clion proj added

---
+++ Hw4/skeleton/cmake-build-debug/CMakeFiles/skeleton.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/elia/Documents/GitHub/opsys/Hw4/skeleton/main.cpp" "C:/Users/elia/Documents/GitHub/opsys/Hw4/skeleton/cmake-build-debug/CMakeFiles/skeleton.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Hw4/skeleton/cmake-build-debug/CMakeFiles/skeleton.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/skeleton.dir/main.cpp.obj"
+  "libskeleton.dll.a"
+  "skeleton.exe"
+  "skeleton.exe.manifest"
+  "skeleton.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/skeleton.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
